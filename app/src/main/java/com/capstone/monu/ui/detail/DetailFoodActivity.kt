85edf1ccf8cef1ba2b7714358be5f16d3880ce4c
fatwaabdusyukur <waengyoo@gmail.com>
2021-11-29 21:00:51 +0700

    Memperbaiki halaman daily

---
+++ app/src/main/java/com/capstone/monu/ui/detail/DetailFoodActivity.kt
@@ -0,0 +1,11 @@
+package com.capstone.monu.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_food)
+    }
+}
